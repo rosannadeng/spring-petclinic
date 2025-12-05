04186813766c513b93c9fe37984d3d58cce70118
RinKuriakose <98559758+RinKuriakose@users.noreply.github.com>
2025-12-04 20:44:04 -0500

    Update BuildJMVStart.sh
    Signed-off-by: RinKuriakose <98559758+RinKuriakose@users.noreply.github.com>

--- BuildJMVStart.sh
+++ BuildJMVStart.sh
@@ -5,6 +5,7 @@ set -e
 
 docker compose build jenkins
 docker compose build maven-java25
+sudo docker compose build
 docker images | grep -E "jenkins|maven-java25" || {
     echo "ERROR: Jenkins or Maven-Java25 image failed."
     exit 1
